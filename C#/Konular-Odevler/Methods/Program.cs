5cb24edac815446bcfaf22b96ffc8483d1354672
ozan lütfü akaydın <ozanakaydin@gmail.com>
2021-11-10 14:06:24 +0300

    metod overloading added

--- C#/Konular-Odevler/Methods/Program.cs
+++ C#/Konular-Odevler/Methods/Program.cs
@@ -6,6 +6,9 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+
+            /***********Metotların çağrılması**************/
+
             MyClass myClass=new MyClass();
             myClass.Sum(10,23);
 
@@ -22,6 +25,30 @@ namespace Methods
             int arttirTopla=myClass2.ArttırVeTopla(ref z,ref t);
             Console.WriteLine(arttirTopla);
 
+            /***************Out Parametreler**************/
+            // sayi değişkenini int'e çevirmeye çalış.Çevirme başarılıysa (true) outsayi'ya atamasını yap.
+            string sayi="999";
+            bool Sonuc=int.TryParse(sayi,out int outsayi);
+            if(!Sonuc)
+            {
+                Console.WriteLine("Başarısız"+" "+outsayi.GetType());
+            }
+            else
+            {
+                Console.WriteLine("Başarılı"+" "+outsayi.GetType());
+            }
+
+           // Out parametreli metodun çağrılması
+           MyClass myClass3=new MyClass();
+           myClass3.Topla(4,5,out int toplamSonucu);
+           Console.WriteLine(toplamSonucu);
+
+           // Overloading metodun çağrılması
+           MyClass myClass4=new MyClass();
+           int returnResult=myClass4.Cikart(100,50);  // yada
+           int voidResult=myClass4.Cikart(100,50);
+           
+
             
         }
 
@@ -29,6 +56,8 @@ namespace Methods
         {
             Console.WriteLine("Aynı sınıf içinden ulaşmak");
         }
+
+
 
     }
 
@@ -53,7 +82,26 @@ namespace Methods
                 num2++;
                 return num1+num2;
             }
+
+            /****************Out Parametreli Metot****************/
+             public void Topla(int sayi1,int sayi2,out int toplam)
+            {
+                toplam=sayi1+sayi2;
+            }
+
+            /***********Matod Overloading*************/
+            public int Cikart(int number1,int number2)
+            {
+               return number1-number2;
+            }
+
+            public void Cikart(int mynumber1, int mynumer2)
+            {
+                    mynumber1-mynumer2;
+            }
+
     }
+
 }
     
 
