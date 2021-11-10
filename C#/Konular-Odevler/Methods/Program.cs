32babe2b4a6129170ee77ed57036d767402c3314
ozan lütfü akaydın <ozanakaydin@gmail.com>
2021-11-10 15:50:46 +0300

    ok

--- C#/Konular-Odevler/Methods/Program.cs
+++ C#/Konular-Odevler/Methods/Program.cs
@@ -16,10 +16,6 @@ namespace Methods
             int myResult=myClass1.SumSecond(20,12);
             Console.WriteLine(myResult);
 
-            Program a=new Program();
-            a.ToWrite();
-
-
             int z=5,t=10;
             MyClass myClass2=new MyClass();
             int arttirTopla=myClass2.ArttırVeTopla(ref z,ref t);
@@ -27,6 +23,7 @@ namespace Methods
 
             /***************Out Parametreler**************/
             // sayi değişkenini int'e çevirmeye çalış.Çevirme başarılıysa (true) outsayi'ya atamasını yap.
+            /*
             string sayi="999";
             bool Sonuc=int.TryParse(sayi,out int outsayi);
             if(!Sonuc)
@@ -37,6 +34,7 @@ namespace Methods
             {
                 Console.WriteLine("Başarılı"+" "+outsayi.GetType());
             }
+            */
 
            // Out parametreli metodun çağrılması
            MyClass myClass3=new MyClass();
@@ -45,62 +43,138 @@ namespace Methods
 
            // Overloading metodun çağrılması
            MyClass myClass4=new MyClass();
-           int returnResult=myClass4.Cikart(100,50);  // yada
-           int voidResult=myClass4.Cikart(100,50);
+           myClass4.Yazdir("Overloading 1. metot.");
+          
+           
+           // Recursive metodun çağrılması
+           MyClass myClass5=new();
+           Console.WriteLine(myClass5.Expo(3,4));
+
+
+           // Extension metodun çağrılması
+           string ifade="patika c# dersleri";
+           bool result=ifade.CheckSpaces();
+           //
+
+           Console.WriteLine(result); // sonuç true döner.
+           if(result)
+           {
+               Console.WriteLine(ifade.RemoweWhiteSpaces());
+           }
+           //
+           Console.WriteLine(ifade.MakeUpperCase());
+
+           //
+           int [] myArray={3,9,5,1};
+           myArray.SortArray();
+           myArray.WriteDisplay();
+
+           //
+
+           string metin="Merhaba";
+           Console.WriteLine(metin.GetFirstCharacter());
            
 
+           
             
         }
-
-        private void ToWrite()
-        {
-            Console.WriteLine("Aynı sınıf içinden ulaşmak");
-        }
-
-
 
     }
 
     class MyClass
     {
-            public void Sum(int a,int b)
-            {
-                Console.WriteLine(a+b);
-            }
+        public void Sum(int a,int b)
+        {
+            Console.WriteLine(a+b);
+        }
 
-            public int SumSecond(int x,int y)
-            {
-                int result=x+y;
-                return result;
-            }
+        public int SumSecond(int x,int y)
+        {
+            int result=x+y;
+            return result;
+        }
             
             // Ref kullanıldığında ilgili değişkenin kopyaları ile çalışmak yerine orjinal değişkenler üzerinde çalışır.
             // Bellekteki adresleri işaretlenmiş olunur.Referanslar üzerinde değişiklik yapılmış olunur.
-            public int ArttırVeTopla(ref int num1, ref int num2)
-            {
-                num1++;
-                num2++;
-                return num1+num2;
-            }
+        public int ArttırVeTopla(ref int num1, ref int num2)
+        {
+            num1++;
+            num2++;
+            return num1+num2;
+        }
 
-            /****************Out Parametreli Metot****************/
-             public void Topla(int sayi1,int sayi2,out int toplam)
-            {
-                toplam=sayi1+sayi2;
-            }
+        /****************Out Parametreli Metot****************/
+        public void Topla(int sayi1,int sayi2,out int toplam)
+        {
+            toplam=sayi1+sayi2;
+        }
 
-            /***********Matod Overloading*************/
-            public int Cikart(int number1,int number2)
-            {
-               return number1-number2;
-            }
+        /***********Matod Overloading*************/
+        public void Yazdir(string deger)
+        {
+            Console.WriteLine(deger);
+        }
 
-            public void Cikart(int mynumber1, int mynumer2)
-            {
-                    mynumber1-mynumer2;
-            }
+        public void Yazdir()
+        {
+            Console.WriteLine("Overloading 2. metod.");
+        }
+
+
+        /**************Recursive Metotlar (kendi kendini çağıran)*************/
+        public int Expo(int sayim,int ussu)
+        {
+            if(ussu<2)
+            return sayim;
+            return Expo(sayim,ussu-1)*sayim;
+        }
+        
 
     }
+
+    /**************Extension Metotlar*************
+    * Static class içerisinde static olarak tanımlanmalıdırlar
+    * Metot parametresinin önüne "this" keyword'ü getirilmelidir.
+    */
+    public static class Extension
+    {
+        public static bool CheckSpaces(this string param)
+        {
+            return param.Contains(" ");
+        }
+
+        public static string RemoweWhiteSpaces(this string param)
+        {
+            string [] dizi=param.Split(" ");
+            return string.Join("",dizi);
+        }
+
+        public static string MakeUpperCase(this string param)
+        {
+            return param.ToUpper();
+        }
+
+        public static int[] SortArray(this int[] param)
+        {
+            Array.Sort(param);
+            return param;      
+        }
+
+        public static void WriteDisplay(this int [] param)
+        {
+            foreach(int item in param)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static string GetFirstCharacter(this string param)
+        {
+            return param.Substring(0,1);
+        }
+    }
+
+
 
 }
     
