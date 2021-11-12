5169e52dc77009bc02b0a5d4448bcf913b661431
ozan lütfü akaydın <ozanakaydin@gmail.com>
2021-11-12 20:56:00 +0300

    DateTime ve Math metotları eklendi

--- C#/Konular-Odevler/Methods/DateTimeMetotlari/Program.cs
+++ C#/Konular-Odevler/Methods/DateTimeMetotlari/Program.cs
@@ -6,7 +6,61 @@ namespace DateTimeMetotlari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            
+        /**************************DateTime***********************************/
+
+        Console.WriteLine(DateTime.Now); // Tarih-Saat: 12.11.2021 20:18:43
+        Console.WriteLine(DateTime.Now.Date);  // Tarih: 12.11.2021 00:00:00
+        Console.WriteLine(DateTime.Now.Day); // Gün: 12
+        Console.WriteLine(DateTime.Now.Month); // Ay: 11
+        Console.WriteLine(DateTime.Now.Year); // Yıl: 2021
+        Console.WriteLine(DateTime.Now.Hour); // Saat: 20
+        Console.WriteLine(DateTime.Now.Minute); // Dakika: 18
+        Console.WriteLine(DateTime.Now.Second); // Saniye: 43
+
+        Console.WriteLine(DateTime.Now.DayOfWeek); // haftanın hangi günü (string formatta) ==> Friday
+        Console.WriteLine(DateTime.Now.DayOfYear); // yılın kaçıncı günü ==> 316
+        
+        Console.WriteLine(DateTime.Now.ToLongDateString()); // günün uzun tarihi (string)  ==> 12 Kasım 2021 Cuma
+        Console.WriteLine(DateTime.Now.ToShortDateString()); // günün kısa tarihi (string) ==> 12.11.2021
+        Console.WriteLine(DateTime.Now.ToLongTimeString()); // uzun saat formatı (string) ==> 20:18:43
+        Console.WriteLine(DateTime.Now.ToShortTimeString()); // kısa saat formatı (string) ==> 20:18
+
+        Console.WriteLine(DateTime.Now.AddYears(1)); // yıl üzerine ekler ==> Bir yıl sonra: 12.11.2022 20:27:53
+        Console.WriteLine(DateTime.Now.AddDays(2)); // gün üzerine ekler. ==> İki gün sonra: 14.11.2021 20:22:20
+        Console.WriteLine(DateTime.Now.AddHours(5)); // saat üzerine ekler ==> Beş saat sonra: 13.11.2021 01:26:01
+
+
+
+        /**************************Format***********************************/
+
+        Console.WriteLine(DateTime.Now.ToString("dd")); // günün int karşılığı ==> 12
+        Console.WriteLine(DateTime.Now.ToString("ddd")); // günün string kısa karşılığı ==> Cum
+        Console.WriteLine(DateTime.Now.ToString("dddd")); // günün string uzun karşılığı ==> Cuma
+
+        Console.WriteLine(DateTime.Now.ToString("MM")); // ayın int karışığı ==> 11
+        Console.WriteLine(DateTime.Now.ToString("MMM")); // ayın string kısa karşılığı ==> Kas
+        Console.WriteLine(DateTime.Now.ToString("MMMM")); // ayın string uzun karşılığı ==> Kasım
+
+        Console.WriteLine(DateTime.Now.ToString("yy")); // yılın kısa karşılığı ==>21
+        Console.WriteLine(DateTime.Now.ToString("yyyy")); // yılın uzun karşılığı ==> 2021
+      
+
+
+        
+
+
+
+
+        
+
+
+
+
+
+            
         }
     }
 }
--- C#/Konular-Odevler/Methods/MathMetotlari/Program.cs
+++ C#/Konular-Odevler/Methods/MathMetotlari/Program.cs
@@ -6,7 +6,37 @@ namespace MathMetotlari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                /**************Math******************
+                1- Abs (Mutlak değer)
+                2- Pow (üs alma)
+                3- sqrt(karekök alma)
+                4- Ceiling (Yukarı yuvarlar)
+                5- Floor (Aşağı yuvarlar)
+                6- Round (yakına yuvarlar)
+                7- Max (en büyük sayı);
+                8- Min (en küçük sayı);
+
+                */
+
+        Console.WriteLine(Math.Abs(-1)); // ==> 3
+        Console.WriteLine(Math.Pow(5,2)); // ==> 25
+        Console.WriteLine(Math.Sqrt(81)); // ==> 9
+
+        Console.WriteLine(Math.Ceiling(3.50)); // ==> 4
+        Console.WriteLine(Math.Floor(3.50)); // ==> 3
+        Console.WriteLine(Math.Round(3.40)); // ==> 4
+
+        Console.WriteLine(Math.Max(10,15)); // ==> 15
+        Console.WriteLine(Math.Min(10,15)); // ==> 10
+        
+        
+        
+
+
+
+
+
+
         }
     }
 }
